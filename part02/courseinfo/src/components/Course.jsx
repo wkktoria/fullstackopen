20f40e8d6a7020fd9678c4555fdaa9e9db37f412
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-08 17:57:17 +0200

    Complete exercise 2.1: Course information step6

---
+++ part02/courseinfo/src/components/Course.jsx
@@ -0,0 +1,35 @@
+// App
+//   Course
+//     Header
+//     Content
+//       Part
+//       Part
+
+const Header = ({ name }) => <h1>{name}</h1>;
+
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
