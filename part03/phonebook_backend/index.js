f3bc90db311dc0cc456f564d7322932c61369dce
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-15 20:36:38 +0200

    Phonebook: Add changes to make backend works with frontend

--- part03/phonebook_backend/index.js
+++ part03/phonebook_backend/index.js
@@ -36,6 +36,7 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
 app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 
 app.get("/info", (request, response) => {
