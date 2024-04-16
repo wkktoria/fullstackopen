fcf96da3cefa5d3e9f8680b9a2669031de8ddff6
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-16 14:05:41 +0200

    Complete exercise 3.15: Phonebook database, step 3

--- part03/phonebook_backend/index.js
+++ part03/phonebook_backend/index.js
@@ -48,10 +48,11 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => console.error(error.message));
 });
 
 app.post("/api/persons", (request, response) => {
