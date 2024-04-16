15854098df962c5516a37736219562fc2be1b407
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-16 20:56:04 +0200

    Complete exercise 4.4: Helper Functions and Unit Tests, step 2

--- part04/bloglist/utils/list_helper.js
+++ part04/bloglist/utils/list_helper.js
@@ -1,5 +1,8 @@
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => blogs.map((blog) => blog.likes).reduce((accumulator, current) => accumulator + current, 0);
+
 module.exports = {
   dummy,
+  totalLikes,
 };
