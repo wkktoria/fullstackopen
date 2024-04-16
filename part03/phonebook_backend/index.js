bcc48a472b8ef7d87898cf237cd423c7d005c515
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-16 13:53:57 +0200

    Complete exercise 3.14: Phonebook database, step 2

--- part03/phonebook_backend/index.js
+++ part03/phonebook_backend/index.js
@@ -69,25 +69,14 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  if (
-    persons.find(
-      (person) => person.name.toUpperCase() === body.name.toUpperCase(),
-    )
-  ) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 const PORT = process.env.PORT;
