63130521842d44affcb830bc8580117583d0975c
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-16 21:18:07 +0200

    Complete exercise 4.5*: Helper Functions and Unit Tests, step 3

--- part04/bloglist/.eslintrc.js
+++ part04/bloglist/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-  extends: 'airbnb-base',
+  extends: "airbnb-base",
   rules: {
-    'no-console': 0,
+    "no-console": 0,
+    "max-len": { code: 80 },
   },
 };
--- part04/bloglist/utils/list_helper.js
+++ part04/bloglist/utils/list_helper.js
@@ -1,8 +1,28 @@
 const dummy = (blogs) => 1;
 
-const totalLikes = (blogs) => blogs.map((blog) => blog.likes).reduce((accumulator, current) => accumulator + current, 0);
+const totalLikes = (blogs) =>
+  blogs
+    .map((blog) => blog.likes)
+    .reduce((accumulator, current) => accumulator + current, 0);
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+
+  const favorite = blogs.reduce((accumulator, current) =>
+    accumulator.likes > current.likes ? accumulator : current,
+  );
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
