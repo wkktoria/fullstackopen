a18b1fe85c87c0427527e69b43f45280a05bdf40
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-03-20 21:17:18 +0100

    Finish exercise 2.1

---
+++ part02/courseinfo/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.name} name={part.name} exercises={part.exercises} />
+    ))}
+  </div>
+);
+
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
+
+const Total = ({ total }) => <p>Number of exercises {total}</p>;
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={0} />
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
