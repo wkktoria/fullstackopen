ea86b9269e3a408b1df0037eb8f29416bcf96eee
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-03-21 10:25:10 +0100

    Finish exercise 2.6

---
+++ part02/phonebook/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleAddClick = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(newPerson));
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name:
+          <input
+            value={newName}
+            onChange={(event) => handleNameChange(event)}
+          />
+        </div>
+        <div>
+          <button type="submit" onClick={() => handleAddClick(event)}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
