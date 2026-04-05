888ce3771de1498f80bd41581b3fbc7608a351d7
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-04-05 18:50:35 +0200

    Finish exercise 3.2

--- part03/phonebook-backend/index.js
+++ part03/phonebook-backend/index.js
@@ -28,6 +28,13 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  const now = new Date();
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${now}</p>`,
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
