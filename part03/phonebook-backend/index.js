e68322ef8b2203aef6de1846e4b0cd7b9dcf660a
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-04-06 18:06:55 +0200

    Finish exercise 3.3

--- part03/phonebook-backend/index.js
+++ part03/phonebook-backend/index.js
@@ -28,6 +28,17 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 app.get("/info", (request, response) => {
   const now = new Date();
   response.send(
