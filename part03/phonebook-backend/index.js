0787149c29004e8e845b2290fbcd94e29e7fe14c
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-05-08 13:07:38 +0200

    Finish exercise 3.4

--- part03/phonebook-backend/index.js
+++ part03/phonebook-backend/index.js
@@ -39,6 +39,12 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
 app.get("/info", (request, response) => {
   const now = new Date();
   response.send(
